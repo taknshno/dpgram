9aa336d73b456123e411d7ad9acfcfcc81c8b32c
Taiki Nishino <tak.nshno.dic@gmail.com>
2023-04-20 22:41:26 +0900

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,23 @@
+require_relative "boot"
+
+require "rails/all"
+
+Bundler.require(*Rails.groups)
+
+module Dpgram
+  class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
+
+    config.load_defaults 6.1
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.javascript false
+    end
+
+  end
+end
