38d858b36660a6acd179876bc869efa182e672bf
taknshno <126603921+taknshno@users.noreply.github.com>
2023-04-26 13:54:22 +0900

    Generate the Pictures Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-
+  root 'pictures#index'
+  resources 'pictures'
 end
--- db/migrate/20230426034517_create_create_pictures.rb
+++ db/migrate/20230426034517_create_create_pictures.rb
@@ -1,7 +1,8 @@
 class CreateCreatePictures < ActiveRecord::Migration[6.1]
   def change
     create_table :create_pictures do |t|
-
+      t.text :image, null: false
+      t.text :content
       t.timestamps
     end
   end
