69a5c3d884fa3f151c4848a63e3470d0725089cf
taknshno <126603921+taknshno@users.noreply.github.com>
2023-04-26 16:17:42 +0900

    Add partial and helper file for posting

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,4 +6,8 @@ class PicturesController < ApplicationController
   def new
     @picture = Picture.new
   end
+
+  def confirm
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pictures#index'
-  resources :pictures
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
 end
