45baf89a79a9264ffeb896aed55f5053ab2cb8f6
taknshno <126603921+taknshno@users.noreply.github.com>
2023-04-26 18:55:56 +0900

    Add show file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -13,3 +8,36 @@ import '../stylesheets/application';
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(function () {
+  setTimeout(function () {
+    $(".alert").hide();
+  }, 4000);
+});
+
+if (document.URL.match(/new/) || document.URL.match(/edit/) || document.URL.match(/confirm/)) {
+  document.addEventListener("DOMContentLoaded", () => {
+    const createImageHTML = (blob) => {
+      const imageElement = document.getElementById("prv");
+      const blobImage = document.createElement("img");
+      blobImage.setAttribute("src", blob);
+
+      imageElement.appendChild(blobImage);
+    };
+
+    document.getElementById("picture_image").addEventListener("change", (e) => {
+      const imageContent = document.querySelector("#prv").querySelector("img");
+      const textContent = document.querySelector("#prv").querySelector("h5");
+      console.log(imageContent);
+      if (imageContent) {
+        imageContent.remove();
+      } else if (textContent) {
+        textContent.remove();
+      }
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      createImageHTML(blob);
+    });
+  });
+}
