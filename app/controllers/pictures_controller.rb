771d26369a5092a1f21c658ac025fddb97e6f0b3
taknshno <126603921+taknshno@users.noreply.github.com>
2023-04-26 16:41:56 +0900

    Add confirm page

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,13 +1,70 @@
 class PicturesController < ApplicationController
 
   def index
+    @pictures = Picture.all
   end
 
   def new
-    @picture = Picture.new
+    if params[:back]
+      @picture = Picture.new(picture_params)
+    else
+      @picture = Picture.new
+    end
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    # @picture = current_user.pictures.build(picture_params)
+    if params[:back]
+      render :new
+    else
+      if @picture.save
+        flash[:success] = "写真を投稿しました"
+        redirect_to pictures_path
+      else
+        flash[:danger] = "入力に誤りがあります"
+        render :new
+      end
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @picture.update(picture_params)
+      flash[:success] = "記事を編集しました"
+      redirect_to pictures_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @picture.destroy
+    flash[:success] = "記事を削除しました"
+    redirect_to pictures_path
   end
 
   def confirm
-    
+    @picture = Picture.new(picture_params)
+    #@picture = current_user.pictures.build(picture_params)
+    if @picture.invalid?
+      flash[:danger] = "入力に誤りがあります"
+      render :new
+    end
+  end
+
+  private
+
+  def set_picture
+    @picture = Picture.find(params[:id])
+  end
+
+  def picture_params
+    params.require(:picture).permit(:image, :image_cache, :content)
   end
 end
