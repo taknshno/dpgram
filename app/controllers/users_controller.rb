c95b0ea494ce760eb25ce8cb64a488949cd911ce
taknshno <126603921+taknshno@users.noreply.github.com>
2023-04-27 00:33:39 +0900

    Add new.html for user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+      flash[:success] = "アカウントを作成しました"
+    else
+      flash[:danger] = "入力に誤りがあります"
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
+  end
 end
