b79b29a96b4bcf4877fbac63170ed02730ce1e3b
taknshno <126603921+taknshno@users.noreply.github.com>
2023-04-27 00:05:55 +0900

    Generate user model and set validates

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,5 @@
 class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
+  validates :image, presence: true
+  validates :content, length: { maximum: 100 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_26_060558) do
+ActiveRecord::Schema.define(version: 2023_04_26_145851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2023_04_26_060558) do
   create_table "pictures", force: :cascade do |t|
     t.text "image", null: false
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.text "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
