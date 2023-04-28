70129d47a3608d77195fc7757f8efb3b52e90b40
taknshno <126603921+taknshno@users.noreply.github.com>
2023-04-29 08:32:14 +0900

    Add login feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,22 @@ class SessionsController < ApplicationController
   def new
     render layout: false
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "ログインしました"
+      redirect_to pictures_path
+    else
+      flash[:danger] = "ログインできませんでした"
+      render :new, layout: false
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:success] = "ログアウトしました"
+    redirect_to new_session_path
+  end
 end
